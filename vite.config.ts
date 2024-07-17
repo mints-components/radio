78df7fd5b67c578aac15fb0f2de287832d8c0405
mintsweet <0x1304570@gmail.com>
2024-07-17 19:30:56 +1200

    chore: remove example and rename package name to radio

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'my-component',
-      fileName: 'my-component',
+      name: 'radio',
+      fileName: 'radio',
     },
     rollupOptions: {
       external: [
